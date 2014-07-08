7549a7c35dfa2027a57b63622075c20ac3c99426
marcokstephen <marcok.stephen@gmail.com>
2014-07-07 21:34:53 -0400

    Updating helpfiles

--- mobile/src/main/java/com/sm/stepsassistant/HelpActivity.java
+++ mobile/src/main/java/com/sm/stepsassistant/HelpActivity.java
@@ -19,7 +19,9 @@ public class HelpActivity extends Activity {
 
         String htmlString = "&#8226; To use this app, you must be actively connected to an Android Wear device.<p>" +
                 " &#8226; To change statistics such as daily step goal, you must change settings on this mobile app<p> " +
-                "&#8226;To request a feature or report a bug, please contact me at: apps@stephenmarcok.com";
+                "&#8226; To request a feature or report a bug, please contact me at: apps@stephenmarcok.com<p> " +
+                "&#8226; After installing, this app might not show the same step count as the Google Fit step counter for up to the first 24 hours of usage<p> " +
+                "&#8226; Counter resets at midnight";
         helpTextView.setText(Html.fromHtml(htmlString));
     }
 }
